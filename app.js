dee3498f2d0c30b6efe754e96af5c5ac34695291
Alkaff <dzaky.alkaff@gmail.com>
2013-02-10 05:18:28 -0500

    Revisi #4

---
+++ app.js
@@ -0,0 +1,99 @@
+
+/**
+ * Module dependencies.
+ */
+var express = require('express')
+  , app = express()
+  , routes = require('./routes')
+  , path = require('path')
+  , server = require('http').createServer(app)
+  , io = require('socket.io').listen(server);
+
+var port = (process.env.VMC_APP_PORT || 3000);
+var host = (process.env.VCAP_APP_HOST || 'localhost');
+var http = require('http');
+
+server.listen(3000, function(){
+  console.log("Express server listening on port " + 3000);
+});
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+
+io.sockets.on('connection', function (socket) {
+  socket.on('add', function (data) {
+    socket.emit('addData', routes.addCol(data));
+  });
+
+  socket.on('update', function (data) {
+    socket.emit('updateData', routes.updateCol(data));
+  });
+
+  socket.on('remove', function (data) {
+    socket.emit('removeData', routes.removeCol(data));
+  });
+
+});
+
+
+if(process.env.VCAP_SERVICES){
+  var env = JSON.parse(process.env.VCAP_SERVICES);
+  var mongo = env['mongodb-2.0'][0]['credentials'];
+}
+else{
+  var mongo = {
+    "hostname":"localhost",
+    "port":27017,
+    "username":"",
+    "password":"",
+    "name":"",
+    "db":"db"
+  }
+}
+
+var generate_mongo_url = function(obj){
+  obj.hostname = (obj.hostname || 'localhost');
+  obj.port = (obj.port || 27017);
+  obj.db = (obj.db || 'test');
+
+  if(obj.username && obj.password){
+    return "mongodb://" + obj.username + ":" + obj.password + "@" + obj.hostname + ":" + obj.port + "/" + obj.db;
+  }
+  else{
+    return "mongodb://" + obj.hostname + ":" + obj.port + "/" + obj.db;
+  }
+}
+
+var mongourl = generate_mongo_url(mongo);
+
+var record_visit = function(req, res){
+  /* Connect to the DB and auth */
+  require('mongodb').connect(mongourl, function(err, conn){
+    conn.collection('employees', function(err, coll){
+      /* Simple object to insert: ip address and date */
+      object_to_insert = { 'ip': req.connection.remoteAddress, 'ts': new Date() };
+
+      /* Insert the object then print in response */
+      /* Note the _id has been created */
+      coll.insert( object_to_insert, {safe:true}, function(err){
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.write(JSON.stringify(object_to_insert));
+        res.end('\n');
+      });
+    });
+  });
+}
